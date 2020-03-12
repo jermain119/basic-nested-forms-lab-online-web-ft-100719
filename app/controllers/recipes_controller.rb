51a0f3993bb1abd37a8eac741dffa5daf0cbefa4
jermain119 <jermain119@gmail.com>
2020-03-12 11:55:29 -0500

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -14,6 +14,7 @@ class RecipesController < ApplicationController
   end
 
   def create
+   
     Recipe.create(recipe_params)
     redirect_to recipes_path
   end
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -20,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160114135651) do
     t.datetime "updated_at", null: false
     t.integer  "recipe_id"
   end
+
 
   create_table "recipes", force: :cascade do |t|
     t.string   "title"
